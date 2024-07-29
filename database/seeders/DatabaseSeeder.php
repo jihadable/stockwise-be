7aed58203b45ea4d273212d22e7260cf2ca42083
jihadable <umarjihad141004@gmail.com>
2024-07-29 13:00:12 +0700

    modify seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,12 @@ class DatabaseSeeder extends Seeder {
      * Seed the application's database.
      */
     public function run(): void {
+        $password = env("PRIVATE_PASSWORD");
+
         User::create([
             "username" => "umar",
             "email" => "umarjihad@gmail.com",
-            "password" => Hash::make("abcddcba"),
+            "password" => Hash::make($password),
             "bio" => "test"
         ]);
     }
