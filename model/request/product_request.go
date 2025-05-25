1da16481b11632ff4ba03d6729d1870725f2fd67
jihadable <umarjihad141004@gmail.com>
2025-05-25 19:07:34 +0700

    fix validate error in product request

--- model/request/product_request.go
+++ model/request/product_request.go
@@ -2,7 +2,7 @@ package request
 
 type ProductRequest struct {
 	Name        string  `json:"name" validate:"required"`
-	Category    string  `json:"category" validate:"category"`
+	Category    string  `json:"category" validate:"required"`
 	Price       float32 `json:"price" validate:"required,gt=0"`
 	Quantity    int     `json:"quantity" validate:"required,min=1"`
 	Description string  `json:"description" validate:"required"`
