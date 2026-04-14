0b0b3e4cd26f5de8cfa7c698f35f81ee237fe91c
jihadable <umarjihad141004@gmail.com>
2026-04-14 10:53:19 +0700

    update product service

--- services/product_service.go
+++ services/product_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/jihadable/stockwise-be/config"
@@ -73,6 +74,7 @@ func (service *ProductServiceImpl) GetProductById(id string) (*entity.Product, e
 		err = json.Unmarshal([]byte(productInRedis), &product)
 
 		if err == nil {
+			fmt.Println("ambil dari redis")
 			return &product, nil
 		}
 	}
